6cd2ec09d04df7c02490836d79ab4e9021517581
Krystian Szutowicz <krystian.szutowicz@boeing.com>
2021-06-18 22:15:42 +0200

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Krystian Szutowicz-EXT on 18/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
